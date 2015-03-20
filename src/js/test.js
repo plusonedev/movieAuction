8a5182758a0b4750ddf17dacf38c9bc698b8dc84
Mike Kelly <kellysystemsrepair@gmail.com>
2015-03-20 14:05:16 -0400

    switched from gulp to grunt

--- src/js/test.js
+++
@@ -1,6 +0,0 @@
-gulp.task('orders_js', function () {
-    gulp.src(orders_js) // Javascript files source defined else where as an array (top of file)
-//      .pipe(uglify())
-        .pipe(uglify().on('error', function(e) { console.log('\x07',e.message); return this.end(); }))
-        .pipe(gulp.dest('assets/js/pages'));
-});
