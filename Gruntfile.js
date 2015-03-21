0e8f89b56d6d73da66047881b54c4a922a25a468
Mike <mike@plusonedevelopments.com>
2015-03-20 21:30:09 -0400

    added js and css minification automation

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,21 +2,52 @@ var grunt = require('grunt');
 
 grunt.loadNpmTasks('grunt-bower-concat');
 grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-contrib-copy');
+
 
 grunt.initConfig({
+    copy: {
+        main: {
+            files: [
+                // includes files within path
+                {expand: true, cwd:'src', src: ['**/*.html'], dest: 'build/', filter: 'isFile'},
+                
+                // includes files within path and its sub-directories
+                //{expand: true, src: ['path/**'], dest: 'dest/'},
+                
+                // makes all src relative to cwd
+                //{expand: true, cwd: 'path/', src: ['**'], dest: 'dest/'},
+                
+                // flattens results to a single level
+                //{expand: true, flatten: true, src: ['path/**'], dest: 'dest/', filter: 'isFile'},
+            ],
+        },
+    },
+    cssmin: {
+        target: {
+            files: [{
+                expand: true,
+                cwd: 'src',
+                src: ['*.css', '!*.min.css'],
+                dest: 'build/css',
+                ext: '.min.css'
+            }]
+        }
+    },
 
     uglify: {
         my_target: {
 	    files: {
-                'build/_bower.min.js': ['build/_bower.js']
+                'build/js/bower.min.js': ['src/bower.js']
             }
         }
     },
 
     bower_concat: {
         all: {
-            dest: 'build/_bower.js',
-            cssDest: 'build/_bower.css',
+            dest: 'src/bower.js',
+            cssDest: 'src/bower.css',
             bowerOptions: {
                 relative: false
             }
@@ -25,7 +56,9 @@ grunt.initConfig({
 
 });
 
-grunt.registerTask('buildbower', ['bower_concat','uglify'] );
+
+grunt.registerTask('importbower', ['bower_concat']);
+grunt.registerTask('build', ['uglify','cssmin','copy'] );
 
 
 
